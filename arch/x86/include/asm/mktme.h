d22c9ec885f266254dd65c638f831c1d3f693415
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2019-05-08 17:43:31 +0300

    x86/mm: Add a helper to retrieve KeyID for a page
    page_ext allows to store additional per-page information without growing
    main struct page. The additional space can be requested at boot time.
    
    Store KeyID in bits 31:16 of extended page flags. These bits are unused.
    
    page_keyid() returns zero until page_ext is ready. page_ext initializer
    enables a static branch to indicate that page_keyid() can use page_ext.
    The same static branch will gate MKTME readiness in general.
    
    We don't yet set KeyID for the page. It will come in the following
    patch that implements prep_encrypted_page(). All pages have KeyID-0 for
    now.
    
    page_keyid() will be used by KVM which can be built as a module. We need
    to export mktme_enabled_key to be able to inline page_keyid().
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/include/asm/mktme.h
+++ arch/x86/include/asm/mktme.h
@@ -2,15 +2,43 @@
 #define	_ASM_X86_MKTME_H
 
 #include <linux/types.h>
+#include <linux/page_ext.h>
+#include <linux/jump_label.h>
 
 #ifdef CONFIG_X86_INTEL_MKTME
 extern phys_addr_t mktme_keyid_mask;
 extern int mktme_nr_keyids;
 extern int mktme_keyid_shift;
+
+DECLARE_STATIC_KEY_FALSE(mktme_enabled_key);
+static inline bool mktme_enabled(void)
+{
+	return static_branch_unlikely(&mktme_enabled_key);
+}
+
+extern struct page_ext_operations page_mktme_ops;
+
+#define page_keyid page_keyid
+static inline int page_keyid(const struct page *page)
+{
+	if (!mktme_enabled())
+		return 0;
+
+	return lookup_page_ext(page)->keyid;
+}
+
+
 #else
 #define mktme_keyid_mask	((phys_addr_t)0)
 #define mktme_nr_keyids		0
 #define mktme_keyid_shift	0
+
+#define page_keyid(page) 0
+
+static inline bool mktme_enabled(void)
+{
+	return false;
+}
 #endif
 
 #endif
--- arch/x86/mm/mktme.c
+++ arch/x86/mm/mktme.c
@@ -9,3 +9,24 @@ phys_addr_t mktme_keyid_mask;
 int mktme_nr_keyids;
 /* Shift of KeyID within physical address. */
 int mktme_keyid_shift;
+
+DEFINE_STATIC_KEY_FALSE(mktme_enabled_key);
+EXPORT_SYMBOL_GPL(mktme_enabled_key);
+
+static bool need_page_mktme(void)
+{
+	/* Make sure keyid doesn't collide with extended page flags */
+	BUILD_BUG_ON(__NR_PAGE_EXT_FLAGS > 16);
+
+	return !!mktme_nr_keyids;
+}
+
+static void init_page_mktme(void)
+{
+	static_branch_enable(&mktme_enabled_key);
+}
+
+struct page_ext_operations page_mktme_ops = {
+	.need = need_page_mktme,
+	.init = init_page_mktme,
+};
