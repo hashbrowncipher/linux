61c3f1a5ab04c2f9e4676b161ea94060ae041200
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2019-05-08 17:43:24 +0300

    mm/page_alloc: Unify alloc_hugepage_vma()
    We don't need to have separate implementations of alloc_hugepage_vma()
    for NUMA and non-NUMA. Using variant based on alloc_pages_vma() we would
    cover both cases.
    
    This is preparation patch for allocation encrypted pages.
    
    alloc_pages_vma() will handle allocation of encrypted pages. With this
    change we don' t need to cover alloc_hugepage_vma() separately.
    
    The change makes typo in Alpha's implementation of
    __alloc_zeroed_user_highpage() visible. Fix it too.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -511,14 +511,10 @@ alloc_pages(gfp_t gfp_mask, unsigned int order)
 extern struct page *alloc_pages_vma(gfp_t gfp_mask, int order,
 			struct vm_area_struct *vma, unsigned long addr,
 			int node, bool hugepage);
-#define alloc_hugepage_vma(gfp_mask, vma, addr, order) \
-	alloc_pages_vma(gfp_mask, order, vma, addr, numa_node_id(), true)
 #else
 #define alloc_pages(gfp_mask, order) \
 		alloc_pages_node(numa_node_id(), gfp_mask, order)
 #define alloc_pages_vma(gfp_mask, order, vma, addr, node, false)\
-	alloc_pages(gfp_mask, order)
-#define alloc_hugepage_vma(gfp_mask, vma, addr, order) \
 	alloc_pages(gfp_mask, order)
 #endif
 #define alloc_page(gfp_mask) alloc_pages(gfp_mask, 0)
@@ -526,6 +522,8 @@ extern struct page *alloc_pages_vma(gfp_t gfp_mask, int order,
 	alloc_pages_vma(gfp_mask, 0, vma, addr, numa_node_id(), false)
 #define alloc_page_vma_node(gfp_mask, vma, addr, node)		\
 	alloc_pages_vma(gfp_mask, 0, vma, addr, node, false)
+#define alloc_hugepage_vma(gfp_mask, vma, addr, order) \
+	alloc_pages_vma(gfp_mask, order, vma, addr, numa_node_id(), true)
 
 extern unsigned long __get_free_pages(gfp_t gfp_mask, unsigned int order);
 extern unsigned long get_zeroed_page(gfp_t gfp_mask);
